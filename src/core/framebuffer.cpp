d4cfc58c2bb85035f651ffc371f16ea087d6c2dd
CptTony303 <anton.hartmann98@gmail.com>
2024-03-26 06:30:18 +0100

    unfassbar, es funktioniert

--- src/core/framebuffer.cpp
+++ src/core/framebuffer.cpp
@@ -16,7 +16,7 @@ Framebuffer::Framebuffer(int width, int height) {
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	//get actual screen size
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_FLOAT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
